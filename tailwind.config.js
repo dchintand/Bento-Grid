9aab6e5c12bde84294b65b193665c2eda1c13954
Chintan Dobariya <dchintand@gmail.com>
2024-12-31 15:11:07 +0530

    Intial comit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./**/*.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        dmsans: ['DM Sans','sans serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
